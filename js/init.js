7027357cde86234ef5bf2dae3729f36d908a7c51
kunioarlanda <arlanda.kunio@gmail.com>
2023-01-22 17:05:05 +0700

    commit version v1.0.1

--- js/init.js
+++ js/init.js
@@ -448,7 +448,7 @@ function cavani_tm_contact_form(){
 		}
 		else{
 			// Returns successful data submission message when the entered information is stored in database.
-			jQuery.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
+			jQuery.post("https://formsubmit.co/ajax/kunioarlanda@gmail.com",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
 				
 				jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
 				
