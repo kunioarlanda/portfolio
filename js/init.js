b4a457e7b7118dba4cc7ec99b710507abfe782df
kunioarlanda <arlanda.kunio@gmail.com>
2023-01-22 18:48:30 +0700

    contact form

--- js/init.js
+++ js/init.js
@@ -442,21 +442,21 @@ function cavani_tm_contact_form(){
 	
 		jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
 		//checking for blank fields	
-		if(name===''||email===''||message===''){
+		if(name===''||email===''||subject===''||message===''){
 			
 			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
 		}
 		else{
 			// Returns successful data submission message when the entered information is stored in database.
-			jQuery.post("https://formsubmit.co/83a77d2f2a64fc4e024d5433096ccd48",{ name: name, email: email, message:message, subject: subject}, function(data) {
+			jQuery.post("https://formsubmit.co/ajax/83a77d2f2a64fc4e024d5433096ccd48",{ name: name, email: email, subject: subject, message:message}, function(data) {
 				
-				jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
+				jQuery(".contact_form .returnmessage");//Append returned message to message paragraph
 				
 				
 				if(jQuery(".contact_form .returnmessage span.contact_error").length){
 					jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);		
 				}else{
-					jQuery(".contact_form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
+					jQuery(".contact_form .returnmessage").append(success);
 					jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
 				}
 				
